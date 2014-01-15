c7422da7b41ab72b5afe376d84bf838e8b2692ef
Sidhant Godiwala <grinnbearit@liara.local>
2014-01-15 21:32:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# spelunky
+
+A Clojure library to parse the bitcoin blockchain
+
+## License
+
+Copyright © 2014 Sidhant Godiwala (grinnbearit)
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject spelunky "0.1.0"
+  :description "A bitcoin blockchain parser"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojurewerkz/buffy "1.0.0-beta1"]])
